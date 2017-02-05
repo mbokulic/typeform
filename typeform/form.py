99f860ff920fee078babaff69ae8bb2fb060293f
marko <bokmarko@gmail.com>
2017-02-05 19:25:35 +0100

    refactor: moved transformation functions into methods from class TypeForm

--- typeform/form.py
+++ typeform/form.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from . import config
 from datetime import datetime
 from collections import OrderedDict
 import copy
+import unicodedata
+from string import ascii_letters
+import re
 
 
 class TypeForm:
@@ -35,6 +39,107 @@ class TypeForm:
         for question in questions:
             questions_dict[question["id"]] = question['question']
         return questions_dict
+
+    def get_transformed_questions(self):
+        """
+        formats questions from typeform API
+        :returns: a list of dicts that represent questions for this typeform
+        """
+        questions = self.get_questions()
+        return self.transform_questions(questions)
+
+    def transform_questions(self, questions):
+        '''
+        formats questions from typeform API
+        :param questions: list of raw questions
+        :returns: a list of dicts that represent questions for this typeform
+        '''
+        idx = 0
+        choice_id = ''
+        letters = list(ascii_letters)
+        result = []
+
+        for q_idx in range(len(questions)):
+            increment_counter = True
+            append_question = True
+
+            q = questions[q_idx]
+            q['typeform_id'] = q['id']
+            q['question'] = self.clean_question_text(q['question'])
+            q['type'] = self.parse_question_type(q['typeform_id'])
+
+            suffix = ''
+            if('group' in q.keys()):
+                suffix = letters.pop(0)
+                increment_counter = False
+
+            if(q['type'] == 'group'):
+                letters = list(ascii_letters)
+                if(not config.KEEP_NONQUESTIONS):
+                    append_question = False
+
+            if(q['type'] == 'statement'):
+                if(not config.KEEP_NONQUESTIONS):
+                    append_question = False
+                increment_counter = False
+
+            if(q['type'] == 'choice(multiple answers)'):
+                if(q['field_id'] == choice_id):
+                    increment_counter = False
+                    append_question = False
+                else:
+                    choice_id = q['field_id']
+
+            if(increment_counter):
+                idx += 1
+            q['id'] = self.create_question_id(idx, q['type'], suffix)
+
+            if(append_question):
+                result.append(q)
+
+        return result
+
+    def create_question_id(self, idx, type, suffix=''):
+        if(type in config.TYPE_PREFIXES.keys()):
+            prefix = config.TYPE_PREFIXES[type]
+        else:
+            prefix = config.TYPE_PREFIXES['default']
+        formatter = prefix + '{:0>2}' + suffix
+
+        return formatter.format(str(idx))
+
+    def parse_question_type(self, id_string):
+        match = re.search('[A-z]+', id_string)
+        type = id_string[match.start():match.end() - 1]
+        if(type in config.ALT_TYPES.keys()):
+            type = config.ALT_TYPES[type]
+        if(type == 'list'):
+            if('list_multiple' in config.ALT_TYPES.keys() and
+               'list_single' in config.ALT_TYPES.keys()):
+                match = re.search('choice_[0-9]+', id_string)
+                if(match):
+                    type = config.ALT_TYPES['list_multiple']
+                else:
+                    type = config.ALT_TYPES['list_single']
+        return type
+
+    def clean_question_text(self, html):
+        # removes unicode codes like \xa0
+        clean = unicodedata.normalize('NFKD', html)
+        # replaces <br> with line breaks
+        clean = re.sub('<br ?/>', '\n', clean)
+        # removes html tags
+        clean = re.sub('<.*?>', '', clean)
+        return(clean)
+
+    def questions_to_markdown(self):
+        questions = self.get_transformed_questions()
+        md_list = []
+        for q in questions:
+            md_list.append(' '.join(['#', q['id']]))
+            md_list.append(q['question'] + '\n')
+        md_string = '\n'.join(md_list)
+        return(md_string)
 
     def get_all_completed_responses(self):
         """
